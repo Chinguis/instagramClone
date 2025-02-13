9239cb7b6728b2d9374d825feca8299221445178
Chinguis Umirbayev <chinguis.umirbayev@usequantum.com>
2025-02-12 23:33:40 -0500

    get posts and profile from backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import Post from '../components/Post'
-import { getPosts } from '../services/service'
 import logo from '../../public/logo.png'
 import { Link } from 'react-router';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-    const posts = getPosts();
-    console.log(posts);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/post/all", {
+            method: "GET",
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        }).then(data => setPosts(data))
+        .catch(error => {
+            console.log("Error:", error);
+        });
+    }, []);
+
     const postElements = posts.map((post) => {
         return (
             <Post 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,43 @@
 import { useParams } from 'react-router'
-import { getPostsByUserId, getProfile } from '../services/service'
 import Post from '../components/Post'
 import logo from '../../public/logo.png'
 import { Link } from 'react-router'
+import { useState, useEffect } from 'react'
 
 export default function Profile() {
     const params = useParams();
     const userId = Number(params.userId);
 
-    const profile = getProfile(userId);
+    const [profile, setProfile] = useState(null);
+    const [posts, setPosts] = useState([]);
+    
+    useEffect(() => {
+        fetch("http://localhost:8080/api/profile/" + userId, {
+            method: "GET"
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        }).then(data => setProfile(data))
+        .catch(error => {
+            console.log("Error:", error);
+        });
+    }, []);
 
-    const posts = getPostsByUserId(userId);
+    useEffect(() => {
+        fetch("http://localhost:8080/api/post/" + userId, {
+            method: "GET"
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        }).then(data => setPosts(data))
+        .catch(error => {
+            console.log("Error:", error);
+        });
+    }, []);
 
     const postElements = posts.map((post) => {
         return (
